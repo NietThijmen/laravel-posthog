df83ed3165d85c9736598a323820d2adaef49a86
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2026-03-10 00:45:05 +0100

    chore: remove optional tracing settings from PostHog configuration

--- config/posthog.php
+++ config/posthog.php
@@ -3,10 +3,4 @@
 return [
     'host' => env('POSTHOG_HOST', 'https://eu.i.posthog.com'),
     'api_key' => env('POSTHOG_PROJECT'),
-
-    // Optional tracing settings, turn on and off based on your needs.
-    'tracing' => [
-        'automatic_auth_tracing' => env('POSTHOG_AUTOMATIC_AUTH_TRACING', false),
-        'automatic_route_tracing' => env('POSTHOG_AUTOMATIC_ROUTE_TRACING', false),
-    ],
 ];
